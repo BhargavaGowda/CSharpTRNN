d0c87602481498d4491deba241317f4f51e00281
BhargavaGowda <71914822+BhargavaGowda@users.noreply.github.com>
2022-07-05 11:06:14 +1000

    startingout

---
+++ CTRNN.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class CTRNN{
+
+    float[] potentials;
+    int numOutputs;
+    int numInputs;
+
+    public CTRNN(size, numOutputs, numInputs){
+        potentials = new float[size];
+        
+    }
+}
